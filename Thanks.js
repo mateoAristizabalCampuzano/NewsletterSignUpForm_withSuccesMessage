3ea7eace561d426e6ebaca10fb40465d69398830
Mateo Aristizabal <mateoteo5657@gmail.com>
2023-09-18 09:53:25 -0500

    Primer comentario

---
+++ Thanks.js
@@ -0,0 +1,7 @@
+let variable = localStorage.getItem('miVariable');
+
+document.getElementById('parrafo').innerHTML = `A confirmation email has been sent to <strong>${variable}</strong> Please open it and click the button inside to confirm your subscription`
+
+function volver(){
+    window.location="index.html";
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+
+let correoElement = document.getElementById("inputEmail");
+let expr = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function validarForm(event){
+    event.preventDefault(); 
+    let correo = correoElement.value;
+    if (!expr.test(correo)){                                                          
+        document.getElementById("error-message").style.display = "block";
+        correoElement.style.borderColor = "red";
+        correoElement.style.backgroundColor = "#FFE9E6"; // Un rojo claro
+        return false;
+    } else {
+        document.getElementById("error-message").style.display = "none";
+        correoElement.style.borderColor = "";
+        correoElement.style.backgroundColor = "";
+        localStorage.setItem('miVariable', correo);
+        window.open("Thanks.html", "_self");  // "_self" abre en la misma ventana/tab, "_blank" abre en una nueva ventana/tab
+    }
+}
